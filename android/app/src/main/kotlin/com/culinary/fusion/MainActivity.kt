2c99635cc2bd2532c3c52440dbc43ce1053157ef
alfonsoroberto532-ctrl <alfonsoroberto532@gmail.com>
2026-08-25 02:42:19 +0000

    arreglos para prueba

---
+++ android/app/src/main/kotlin/com/culinary/fusion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.culinary.fusion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.varanova.poscaja
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity : FlutterActivity()
